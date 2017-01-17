4079c4fc92e542b76ea503f1c818318696c50bac
Ilia Khablak <apella2010@gmail.com>
2017-01-17 17:34:36 +0800

    finished project profile page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,6 +17,31 @@ class ProjectsController < ApplicationController
 		end
 	end
 
+	def show_proj
+		a = Project.find(params['id'])
+		render :json => a
+	end
+
+	def update
+    	if params.first[0] == 'languages_required'
+      		a = Project.find(params['id'])
+      		a.languages_required << params.first[1]
+      		a.save
+      		render :json => ["ok"]
+    	elsif params.first[0] == 'killskill'
+      		a = Project.find(params['id'])
+      		a.languages_required.delete("#{params.first[1]}")
+      		if a.save
+        		render :json => ["ok"]
+      		else
+        		render :json => ["no"]
+      		end
+    	else
+      		Project.update(params['id'], params.first[0] => params.first[1])
+      		render :json => ["ok"]
+    	end
+  	end
+
 	def destroy
 		a = Project.find(params['id'])
 		a.destroy
